0fc69b3d6fb737781627fd97f8c3722fbbc5b33e
Vadym Hotko <poipooo600@gmail.com>
2024-09-24 23:08:08 +0100

    "added loop"

--- main.py
+++ main.py
@@ -8,6 +8,37 @@ df = pd.read_csv('GoogleApps.csv')
 
 # print(df.describe())
 # print(df.info())
-print(df[df['Rating']>4.9]['Installs'].plot())
-df['Size'].head().plot(kind='hist')
-plt.show()
+# print(df[df['Rating']>4.9]['Installs'].plot())
+# df['Size'].head().plot(kind='hist')
+# plt.show()
+
+# print(df.groupby(by='Category')['Rating'].mean().plot(kind='pie'))
+# plt.show()
+
+ans = input("""Welcome to the dataset analytics!!!
+0 - exit
+1 - show general statistics about dataset
+2 - show the most popular category
+3 get your preferance statistic 
+: """)
+
+while ans != '0':
+    if ans == '1':
+        print(df.describe().plot())
+    elif ans == '2':
+        temp = df.groupby(by='Category')['Installs'].mean()
+        print(temp)
+        temp.plot(kind='pie')
+    elif ans == '1':
+        print(df.describe().plot())
+    else:
+        print('invalid input, please type in only digits')
+        
+    plt.show()
+    
+    ans = input("""Welcome to the dataset analytics!!!
+0 - exit
+1 - show general statistics about dataset
+2 - show the most popular category
+3 - get your preferance statistic 
+: """)
