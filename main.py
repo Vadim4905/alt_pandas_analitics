33b7abd2ba3364e280e93ffb2261c332270395be
Vadym Hotko <poipooo600@gmail.com>
2024-09-27 22:18:21 +0100

    added preferance statistic functional

--- main.py
+++ main.py
@@ -24,13 +24,26 @@ ans = input("""Welcome to the dataset analytics!!!
 
 while ans != '0':
     if ans == '1':
-        print(df.describe().plot())
+        print(df.describe())
     elif ans == '2':
         temp = df.groupby(by='Category')['Installs'].mean()
         print(temp)
         temp.plot(kind='pie')
-    elif ans == '1':
-        print(df.describe().plot())
+    elif ans == '3':
+        category = input('type in what property would you like to see: ')
+        try:
+            df[category]
+        except:
+            continue
+        aggfunc = input('type in prefered aggrigation fuction (mean or median): ')
+        if aggfunc =='mean':
+            temp = df.groupby(by='Category')[category].mean()
+        elif aggfunc =='median':
+            temp = df.groupby(by='Category')[category].median()
+        else:
+            continue    
+        print(temp)
+        temp.plot(kind='pie')
     else:
         print('invalid input, please type in only digits')
         
